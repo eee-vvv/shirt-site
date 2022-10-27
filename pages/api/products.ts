90a6de5ee2db03e3c805485c5f10c64fd8056bc1
e <info@e-anderson.dev>
2022-10-27 17:45:54 -0400

    get product POST request working (ONLY when all data is validated)

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,10 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { handleGetUnsoldProducts, handlePostProduct } from '../../db/queryHandlers'
+import {
+  handleGetUnsoldProducts,
+  handlePostProduct,
+} from '../../db/queryHandlers';
 
-import { Product, isProduct, isProductJSON } from '../../interfaces'
+import { Product, isProduct, isProductJSON } from '../../interfaces';
 
 type Data = {
-  products: Array<Product>
+  products: Array<Product>;
 };
 
 export default function allProductHandler(
@@ -16,41 +19,42 @@ export default function allProductHandler(
       handleGetUnsoldProducts()
         .then((products) => {
           if (!products) {
-            res.status(500)
-            return
+            res.status(500);
+            return;
           }
           res.status(200).json({ products: products });
         })
         .catch((e) => console.log(e));
       break;
     case 'POST':
-      let postProduct: Product
-      if (isProductJSON(req.body)){
+      let postProduct: Product;
+      if (isProductJSON(req.body)) {
         postProduct = {
           name: req.body.name,
           price: parseInt(req.body.price),
           measurements: req.body.measurements,
           description: req.body.description,
           sold: req.body.sold === 'true',
-          imagesId: req.body.imagesId
-        }
+          imagesId: req.body.imagesId,
+        };
       } else {
-        res.status(400)
-        break
+        // TODO: figure out why requests are hanging
+        console.log('JSON not a product')
+        res.status(400);
+        break;
       }
-      handlePostProduct(postProduct)
-        .then((product) => {
-          if (!product) {
-            res.status(500)
-            return 
-          }
-          res.status(200).json({ products: [product] });
+      handlePostProduct(postProduct).then((product) => {
+        if (!product) {
+          res.status(500);
+          return;
         }
-        )
-      break
+        res.status(200).json({ products: [product] });
+      });
+      break;
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['GET', 'POST']);
       res.status(405).end(`Method ${req.method} Not Allowed`);
+      break;
   }
 }
 
