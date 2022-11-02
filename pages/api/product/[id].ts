fcee69abbdcb160ab62c68f92d5f12ee0caf39aa
JSimple <josephinemayasimple@gmail.com>
2022-11-02 16:27:49 -0400

    added error handling for when delete product id is not in db

--- pages/api/product/[id].ts
+++ pages/api/product/[id].ts
@@ -1,10 +1,11 @@
 import { Client } from 'pg'
 import { Product } from '../../../interfaces'
+import { handleDeleteProduct } from '../../../db/queryHandlers';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function singleProductHandler(
+export default async function singleProductHandler(
   req: NextApiRequest,
-  res: NextApiResponse<Product>
+  res: NextApiResponse<Product|{message:String}>
 ) {
   const {
     query,
@@ -26,8 +27,16 @@ export default function singleProductHandler(
         res.status(200).json({ ...product });
       });
       break;
+    case 'DELETE':
+      const productsDeleted = await handleDeleteProduct(id)
+      if (productsDeleted){
+        res.status(200).json({message: `product deleted`})
+      } else {
+        res.status(400).json({message: `product with id# ${id} not in database`})
+      }
+      break
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['GET','DELETE']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
