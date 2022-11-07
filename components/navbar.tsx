464527bc4db8b3459717a584fb166b1a62cc54c1
e <info@e-anderson.dev>
2022-11-06 19:48:57 -0500

    cart thumbnail boilerplate style

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,22 @@
-import Link from "next/link";
-import React from "react";
-import styles from '../styles/Navbar.module.css'
+import Link from 'next/link';
+import React from 'react';
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav className={styles.container}>
-      <Link href="/">
-        <a className={styles.navLink}>Products</a>
-      </Link>
-      <Link href={`/info`}>
-        <a className={styles.navLink}>About</a>
-      </Link>
-    </nav>
+    <div className={styles.container}>
+      <nav className={styles.navContainer}>
+        <Link href="/">
+          <a className={styles.navLink}>Products</a>
+        </Link>
+        <Link href={`/info`}>
+          <a className={styles.navLink}>About</a>
+        </Link>
+      </nav>
+      <div className={styles.cartThumbnail}>
+        Cart
+      </div>
+    </div>
   );
 };
 
