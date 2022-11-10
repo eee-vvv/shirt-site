e8bfbab7aebd76d0aa76a55bb0a6032783cd4d36
e <info@e-anderson.dev>
2022-11-09 20:30:56 -0500

    basic style stuff

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,25 +1,42 @@
-import { useContext } from "react"
-import { CartContext, ProductsContext } from "../lib/context"
+import { useContext } from 'react';
+import { Product } from '../interfaces';
+import { CartContext, ProductsContext } from '../lib/context';
+import styles from '../styles/Cart.module.css'
 
-
+type CartShirtProps = {
+  product: Product
+}
 
 const Cart = () => {
-  const [cartContext, setCartContext]= useContext(CartContext)
-  const productsContext = useContext(ProductsContext)
+  const [cartContext, setCartContext] = useContext(CartContext);
+  const productsContext = useContext(ProductsContext);
   return (
-    <div>
-      <h1>Cart Open</h1>
-      <ul>
-        {
-        cartContext.map(id => 
-        {
-        const product = productsContext.find(product => product.id === id)
-        return <li key={id}>{product?.name}</li>
-        }
-        )}
-      </ul>
+    <div className={styles.container}>
+      <div className={styles.header}>Cart</div>
+      <div className={styles.shirtList}>
+        {cartContext.map((id) => {
+          const product = productsContext.find((product) => product.id === id);
+          console.log(`product ${id}:`)
+          console.log(product)
+
+          if (!product) return <li>Product not found...</li>
+
+          return <CartShirt key={product.id} product={product} />
+        })}
+      </div>
+    </div>
+  );
+};
+
+const CartShirt = ({ product }: CartShirtProps) => {
+  return (
+    <div className={styles.row}>
+      <button>X</button>
+      <div>IMG</div>
+      <div>{product.name}</div>
+      <div>${product.price}</div>
     </div>
   )
 }
 
-export default Cart
+export default Cart;
