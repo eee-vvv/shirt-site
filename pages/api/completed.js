7cb4bc2193d451d630ddb786308820364c769e1b
e <info@e-anderson.dev>
2022-12-09 18:53:32 -0500

    update boilerplate

--- pages/api/completed.js
+++ pages/api/completed.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       console.log('body: ', req.body);
-      res.status(200);
+      res.status(200).json({ message: 'heard...' });
     } catch (err) {
       console.error('error: ', err.message);
       res.status(err.statusCode || 500).json(err.message);
