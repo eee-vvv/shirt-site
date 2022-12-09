ef0c066155510866478d4363065a25dbc39220c5
e <info@e-anderson.dev>
2022-12-09 17:16:14 -0500

    api/completed boilerplate

---
+++ pages/api/completed.js
@@ -0,0 +1,16 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      console.log('body: ', req.body);
+      res.status(200);
+    } catch (err) {
+      console.error('error: ', err.message);
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
