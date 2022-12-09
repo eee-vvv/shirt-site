c20470e62f7ee543979d211a8312853c8e30e8bf
e <info@e-anderson.dev>
2022-12-08 20:06:53 -0500

    add react-hot-toast for notifications

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,24 @@
-import Navbar from './navbar'
-import Footer from './footer'
+import Navbar from './navbar';
+import Footer from './footer';
 import React from 'react';
 import Logo from './Logo';
 import CheckoutStatusHandler from './CheckoutStatusHandler';
 
-
+import { Toaster } from 'react-hot-toast';
 
 type LayoutProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Toaster />
       <CheckoutStatusHandler />
       <Navbar />
       <Logo />
       <main>{children}</main>
       <Footer />
     </>
-  )
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import React, { useContext, useEffect } from 'react';
 import { useState } from 'react';
 import { CartContext, ProductsContext } from '../lib/context';
 import styles from '../styles/Navbar.module.css';
+import toast from 'react-hot-toast';
 
 import Cart from './Cart';
 
@@ -10,27 +11,23 @@ const Navbar = () => {
   const [showCart, setShowCart] = useState(false);
   const [cartContext, setCartContext] = useContext(CartContext);
   const productsContext = useContext(ProductsContext);
-  const [soldItem, setSoldItem] = useState(false);
   const cartLength = cartContext.length;
 
   useEffect(() => {
     const tmpCart = cartContext.filter((id) => {
       const product = productsContext.find((product) => product.id === id);
       if (!product || product.sold) {
-        setSoldItem(true);
+        toast(
+          'An item from your cart has sold. It has been removed from your cart.'
+        );
         return false;
       } else {
         return true;
       }
     });
-    setCartContext(tmpCart);
-  }, []);
 
-  const handleMissingOrSoldProduct = (id: number) => {
-    const tmpCart = [...cartContext];
-    setCartContext(tmpCart.filter((cartId) => cartId !== id));
-    setSoldItem(true);
-  };
+    setCartContext(tmpCart);
+  }, [productsContext]);
 
   const handleToggleCart = () => {
     setShowCart((prev) => !prev);
@@ -46,10 +43,6 @@ const Navbar = () => {
           <a className={styles.navLink}>About</a>
         </Link>
       </nav>
-      <div className={styles.soldNotice}>
-        One or more items in your cart have sold. It has been updated
-        accordingly.
-      </div>
       {showCart ? (
         <Cart toggle={handleToggleCart} />
       ) : (
